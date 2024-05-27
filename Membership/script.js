7594b44b87cc33af2d01a98c5db9b0d4e297330c
Onrkya <142229574+Onrkya@users.noreply.github.com>
2024-05-27 21:09:22 +0300

    Delete Membership directory

--- Membership/script.js
+++
@@ -1,9 +0,0 @@
-function register() {
-    var fullname = document.getElementById('fullname').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    // You can add your registration logic here, like sending data to server or storing in local storage
-    
-    alert("Registration successful! Welcome, " + fullname + "!");
-}
